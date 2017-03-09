608ce1d445445d644f443fb34feac37057b703f7
hagne <hagen@hagne.net>
2017-03-09 11:12:03 -0700

    new: mode selection implemented new: more products ... with mode selection

--- hagpack/projects/arm/my_products/fRH/from_acsm/v01.py
+++ hagpack/projects/arm/my_products/fRH/from_acsm/v01.py
@@ -41,12 +41,12 @@ class Product(object):
 
     ToDo
     ----
-    separate accumulation from coarse mode and treat separately ... maybe take difference between 200 and 600 growthmode
-    as the growthmode for the coarse mode?!?
+    use growthmode difference between 200 and 600 growthmode as the growthmode for the coarse mode?!?
 
     Change log
     ----------
-    2017-02-28: initially completed"""
+    2017-02-28: initially completed
+    2017-03-02: mode analysis implemented"""
 
     def __init__(self, data_quality='patchy',
                  diameter_cutoff='1um',
@@ -54,6 +54,8 @@ class Product(object):
                  RH_dry=0,
                  RH_wet=85,
                  refractive_index='aosacsm',
+                 mode_analysis = False,
+                 kappa_coarse = None,
                  folder_out= '/Volumes/HTelg_4TB_Backup/arm_data/my_products/fRH/',
                  folder_tdmaaps= '/Users/htelg/data/ARM/SGP/tdmaaps/',
                  folder_acsm= '/Users/htelg/data/ARM/SGP/acsm/',
@@ -65,7 +67,17 @@ class Product(object):
         self.RH_dry = RH_dry
         self.RH_wet = RH_wet
         self.wavelength = wavelength
-        self.refractive_index = refractive_index
+        self.mode_analysis = mode_analysis
+        self.kappa_coarse = kappa_coarse
+        if mode_analysis:
+            if type(refractive_index) != tuple:
+                raise TypeError('if mode_analysis is true refractive_index must be a tuple, e.g. ("aosacsm", 1.5)')
+            if not kappa_coarse:
+                raise ValueError('if mode analysis is true kappa_coarse has to be set')
+            self.refractive_index = refractive_index[0]
+            self.refractive_index_coarse =  refractive_index[1]
+        else:
+            self.refractive_index = refractive_index
         self.folder_out = folder_out
         self.folder = folder_tdmaaps
         self.folder_acsm = folder_acsm
@@ -78,18 +90,61 @@ class Product(object):
             self.intres = {}
 
     def _calculate_one(self, tdmaapssize, kappa, refractive_index, diameter_cutoff,
-                       wavelength, RH_dry, RH_wet):
+                       wavelength, RH_dry, RH_wet, mode_analysis = None):
 
         if diameter_cutoff == '1um':
             dcoff = 1000
         elif diameter_cutoff == '10um':
             dcoff = 10000
 
-        dist = tdmaapssize.size_distribution.zoom_diameter(end=dcoff)
-        dist.parameters4reductions.refractive_index = refractive_index
-        dist.parameters4reductions.wavelength = wavelength
-        dist.parameters4reductions.kappa = kappa
-        fRH = dist.hygroscopicity._get_f_RH(RH_dry, RH_wet)
+        dist = tdmaapssize.size_distribution.copy()
+
+        # mode_analysis = True
+        if mode_analysis:
+            refractive_index_aiken_accu, refractive_index_coarse = refractive_index
+            kappa_aiken_accu, kappa_coarse = kappa
+            # mode selection and parameter setting
+            dist = dist.convert2dVdlogDp()
+            ## aiken accu
+            dist_aiken_accu = dist.mode_analysis.size_dist_accu + dist.mode_analysis.size_dist_aiken
+            dist_aiken_accu = dist_aiken_accu.zoom_diameter(end=dcoff)
+            dist_aiken_accu.parameters4reductions.refractive_index = refractive_index_aiken_accu
+            dist_aiken_accu.parameters4reductions.wavelength = wavelength
+            dist_aiken_accu.parameters4reductions.kappa = kappa_aiken_accu
+
+            ## coarse
+            dist_coarse = dist.mode_analysis.size_dist_coarse
+            dist_coarse = dist_coarse.zoom_diameter(end=dcoff)
+            dist_coarse.parameters4reductions.refractive_index = refractive_index_coarse
+            dist_coarse.parameters4reductions.wavelength = wavelength
+            dist_coarse.parameters4reductions.kappa = kappa_coarse
+
+            #scattcoeff
+            ## aiken_accu
+            ### dry
+            dist_aiken_accu.parameters4reductions.RH = RH_dry
+            scattcoeff_dry_aiken_accu = dist_aiken_accu.hygroscopicity.grown_size_distribution.optical_properties.scattering_coeff
+            ### wet
+            dist_aiken_accu.parameters4reductions.RH = RH_wet
+            scattcoeff_wet_aiken_accu = dist_aiken_accu.hygroscopicity.grown_size_distribution.optical_properties.scattering_coeff
+
+            ## coarse
+            ### dry
+            dist_coarse.parameters4reductions.RH = RH_dry
+            scattcoeff_dry_coarse = dist_coarse.hygroscopicity.grown_size_distribution.optical_properties.scattering_coeff
+            ### wet
+            dist_coarse.parameters4reductions.RH = RH_wet
+            scattcoeff_wet_coarse = dist_coarse.hygroscopicity.grown_size_distribution.optical_properties.scattering_coeff
+
+            fRH = (scattcoeff_wet_coarse + scattcoeff_wet_aiken_accu) / (scattcoeff_dry_coarse + scattcoeff_dry_aiken_accu)
+
+
+        else:
+            dist = dist.zoom_diameter(end=dcoff)
+            dist.parameters4reductions.refractive_index = refractive_index
+            dist.parameters4reductions.wavelength = wavelength
+            dist.parameters4reductions.kappa = kappa
+            fRH = dist.hygroscopicity._get_f_RH(RH_dry, RH_wet)
         return fRH
 
     def calculate_new(self):
@@ -140,12 +195,24 @@ class Product(object):
                     print('acsm file missing .... continue')
                 continue
 
-            ior = str(self.refractive_index).replace('.','o')
-            name_addon = '{}_rh{:d}v{:d}_ior{}_{}'.format(self.diameter_cutoff,
-                                                     self.RH_wet,
-                                                     self.RH_dry,
-                                                     ior,
-                                                     self.data_quality)
+            if self.mode_analysis:
+                ior = str(self.refractive_index).replace('.','o')
+                name_addon = '{}_rh{:d}v{:d}_ior{}_{}_kc{}_{}'.format(self.diameter_cutoff,
+                                                         self.RH_wet,
+                                                         self.RH_dry,
+                                                         ior,
+                                                         '{}j{}'.format(self.refractive_index_coarse.real, self.refractive_index_coarse.imag),
+                                                         self.kappa_coarse,
+                                                         self.data_quality)
+                name_addon = name_addon.replace('.','o')
+            else:
+                ior = str(self.refractive_index).replace('.', 'o')
+                name_addon = '{}_rh{:d}v{:d}_ior{}_{}'.format(self.diameter_cutoff,
+                                                              self.RH_wet,
+                                                              self.RH_dry,
+                                                              ior,
+                                                              self.data_quality)
+
 
             splitname = _splitup_filename(fname_tdmaapssize)
             site = splitname['site']
@@ -170,9 +237,14 @@ class Product(object):
             elif type(self.refractive_index).__name__ == 'float':
                 refractive_index = self.refractive_index
 
+            if self.mode_analysis:
+                refractive_index = (refractive_index, self.refractive_index_coarse)
+                kappa = (kappa, self.kappa_coarse)
+
             tdmaapssize = _atm_arm.read_cdf(self.folder + fname_tdmaapssize, data_quality=self.data_quality, verbose=verbose)
 
-            fofrh = self._calculate_one(tdmaapssize, kappa, refractive_index, diameter_cutoff=self.diameter_cutoff, wavelength= self.wavelength, RH_dry=self.RH_dry, RH_wet=self.RH_wet)
+            fofrh = self._calculate_one(tdmaapssize, kappa, refractive_index, diameter_cutoff=self.diameter_cutoff,
+                                        wavelength= self.wavelength, RH_dry=self.RH_dry, RH_wet=self.RH_wet, mode_analysis=self.mode_analysis)
             # (self, tdmaapssize, kappa, refractive_index, diameter_cutoff, wavelength, RH_dry, RH_wet)
             if self.keep_data:
                 fofrh_list.append(fofrh)
@@ -186,6 +258,40 @@ class Product(object):
                 self.result = fofrh_cat.close_gaps(verbose=False)
             else:
                 self.result = fofrh_list[0]
+
+### Functions
+###############################
+
+
+def sgpacsm2fofrh_1um_rh85v0_ior1o5_patchy(test = False):
+    out = Product(data_quality='patchy',
+                      diameter_cutoff='1um',
+                      wavelength=550,
+                      RH_dry=0,
+                      RH_wet=85,
+                      refractive_index= 1.5,
+                      folder_out='/Volumes/HTelg_4TB_Backup/arm_data/my_products/fRH/sgpacsm2fofrh/1um_rh85v0_ior1o5_patchy/',
+                      folder_tdmaaps='/Users/htelg/data/ARM/SGP/tdmaaps/',
+                      folder_acsm='/Users/htelg/data/ARM/SGP/acsm/',
+                      keep_data=False,
+                      test=test)
+    return out
+
+def sgpacsm2fofrh_10um_rh85v0_ior1o5_patchy(test = False):
+    out = Product(data_quality='patchy',
+                          diameter_cutoff='10um',
+                          wavelength=550,
+                          RH_dry=0,
+                          RH_wet=85,
+                          refractive_index=1.5,  # ('aosacsm', complex(1.63,0.01)),#"aosacsm"
+                          mode_analysis=False,
+                          kappa_coarse=0.0001,
+                          folder_out='/Volumes/HTelg_4TB_Backup/arm_data/my_products/fRH/sgpacsm2fofrh/10um_rh85v0_ior1o5_patchy/',
+                          folder_tdmaaps='/Users/htelg/data/ARM/SGP/tdmaaps/',
+                          folder_acsm='/Users/htelg/data/ARM/SGP/acsm/',
+                          keep_data=False,
+                          test=test)
+    return out
 
 def sgpacsm2fofrh_1um_rh85v0_ioraosacsm_patchy(test = False):
     out = Product(data_quality='patchy',
@@ -215,16 +321,70 @@ def sgpacsm2fofrh_10um_rh85v0_ioraosacsm_patchy(test = False):
                       test=test)
     return out
 
-def sgpacsm2fofrh_1um_rh85v0_ior1o5_patchy(test = False):
+
+
+def sgpacsm2fofrh_1um_rh85v0_ior1o5_1o63j0o01_kc0o0001_patchy(test = False):
     out = Product(data_quality='patchy',
-                      diameter_cutoff='1um',
-                      wavelength=550,
-                      RH_dry=0,
-                      RH_wet=85,
-                      refractive_index= 1.5,
-                      folder_out='/Volumes/HTelg_4TB_Backup/arm_data/my_products/fRH/sgpacsm2fofrh/1um_rh85v0_ior1o5_patchy/',
-                      folder_tdmaaps='/Users/htelg/data/ARM/SGP/tdmaaps/',
-                      folder_acsm='/Users/htelg/data/ARM/SGP/acsm/',
-                      keep_data=False,
-                      test=test)
+                          diameter_cutoff='1um',
+                          wavelength=550,
+                          RH_dry=0,
+                          RH_wet=85,
+                          refractive_index=(1.5, complex(1.63, 0.01)),  # "aosacsm"
+                          mode_analysis=True,
+                          kappa_coarse=0.0001,
+                          folder_out='/Volumes/HTelg_4TB_Backup/arm_data/my_products/fRH/sgpacsm2fofrh/1um_rh85v0_ior1o5_1o63j0o01_kc0o0001_patchy/',
+                          folder_tdmaaps='/Users/htelg/data/ARM/SGP/tdmaaps/',
+                          folder_acsm='/Users/htelg/data/ARM/SGP/acsm/',
+                          keep_data=False,
+                          test=test)
+    return out
+
+
+def sgpacsm2fofrh_10um_rh85v0_ior1o5_1o63j0o01_kc0o0001_patchy(test = False):
+    out = Product(data_quality='patchy',
+                          diameter_cutoff='10um',
+                          wavelength=550,
+                          RH_dry=0,
+                          RH_wet=85,
+                          refractive_index=(1.5, complex(1.63, 0.01)),  # "aosacsm"
+                          mode_analysis=True,
+                          kappa_coarse=0.0001,
+                          folder_out='/Volumes/HTelg_4TB_Backup/arm_data/my_products/fRH/sgpacsm2fofrh/10um_rh85v0_ior1o5_1o63j0o01_kc0o0001_patchy/',
+                          folder_tdmaaps='/Users/htelg/data/ARM/SGP/tdmaaps/',
+                          folder_acsm='/Users/htelg/data/ARM/SGP/acsm/',
+                          keep_data=False,
+                          test=test)
+    return out
+
+
+def sgpacsm2fofrh_1um_rh85v0_ioraosacsm_1o63j0o01_kc0o0001_patchy(test = False):
+    out = Product(data_quality='patchy',
+                          diameter_cutoff='1um',
+                          wavelength=550,
+                          RH_dry=0,
+                          RH_wet=85,
+                          refractive_index=('aosacsm', complex(1.63, 0.01)),  # "aosacsm"
+                          mode_analysis=True,
+                          kappa_coarse=0.0001,
+                          folder_out='/Volumes/HTelg_4TB_Backup/arm_data/my_products/fRH/sgpacsm2fofrh/1um_rh85v0_ioraosacsm_1o63j0o01_kc0o0001_patchy/',
+                          folder_tdmaaps='/Users/htelg/data/ARM/SGP/tdmaaps/',
+                          folder_acsm='/Users/htelg/data/ARM/SGP/acsm/',
+                          keep_data=False,
+                          test=test)
+    return out
+
+def sgpacsm2fofrh_10um_rh85v0_ioraosacsm_1o63j0o01_kc0o0001_patchy(test= False):
+    out = Product(data_quality='patchy',
+                          diameter_cutoff='10um',
+                          wavelength=550,
+                          RH_dry=0,
+                          RH_wet=85,
+                          refractive_index=('aosacsm', complex(1.63, 0.01)),  # "aosacsm"
+                          mode_analysis=True,
+                          kappa_coarse=0.0001,
+                          folder_out='/Volumes/HTelg_4TB_Backup/arm_data/my_products/fRH/sgpacsm2fofrh/10um_rh85v0_ioraosacsm_1o63j0o01_kc0o0001_patchy/',
+                          folder_tdmaaps='/Users/htelg/data/ARM/SGP/tdmaaps/',
+                          folder_acsm='/Users/htelg/data/ARM/SGP/acsm/',
+                          keep_data=False,
+                          test=test)
     return out
--- hagpack/projects/arm/my_products/fRH/from_growth_dist/v01.py
+++ hagpack/projects/arm/my_products/fRH/from_growth_dist/v01.py
@@ -42,12 +42,14 @@ class Product(object):
 
     ToDo
     ----
-    separate accumulation from coarse mode and treat separately ... maybe take difference between 200 and 600 growthmode
+    maybe take difference between 200 and 600 growthmode
     as the growthmode for the coarse mode?!?
 
     Change log
     ----------
-    2017-02-22: initially completed"""
+    - 2017-02-22: initially completed
+    - 2017-03-03: added mode separation. coarse mode parameters are a fixed parameter given during initiation."""
+
 
     def __init__(self, data_quality='patchy',
                  diameter_cutoff='1um',
@@ -56,6 +58,8 @@ class Product(object):
                  RH_dry=0,
                  RH_wet=85,
                  refractive_index ='aosacsm',
+                 mode_analysis = False,
+                 kappa_coarse = None,
                  folder_out='/Users/htelg/data/ARM/myproducts/SGP/',
                  folder_tdmaaps='/Users/htelg/data/ARM/SGP/tdmaaps/',
                  folder_acsm='/Users/htelg/data/ARM/SGP/acsm/',
@@ -72,6 +76,19 @@ class Product(object):
         self.RH_wet = RH_wet
         self.wavelength = wavelength
         self.refractive_index = refractive_index
+
+        self.mode_analysis = mode_analysis
+        self.kappa_coarse = kappa_coarse
+        if mode_analysis:
+            if type(refractive_index) != tuple:
+                raise TypeError('if mode_analysis is true refractive_index must be a tuple, e.g. ("aosacsm", 1.5)')
+            if not kappa_coarse:
+                raise ValueError('if mode analysis is true kappa_coarse has to be set')
+            self.refractive_index = refractive_index[0]
+            self.refractive_index_coarse =  refractive_index[1]
+        else:
+            self.refractive_index = refractive_index
+
         self.black_carbon = None
         self.folder_out = folder_out
         self.folder = folder_tdmaaps
@@ -86,18 +103,72 @@ class Product(object):
             self.intres = {}
 
     def _calculate_one(self, tdmaapssize, tdmahyg, refractive_index, diameter_cutoff,
-                       hygroscopicity_diameter, wavelength, RH_dry, RH_wet):
+                       hygroscopicity_diameter, wavelength, RH_dry, RH_wet, mode_analysis = None, kappa_coarse = None):
 
         if diameter_cutoff == '1um':
             dcoff = 1000
         elif diameter_cutoff == '10um':
             dcoff = 10000
 
-        dist = tdmaapssize.size_distribution.zoom_diameter(end=dcoff)
-        dist.parameters4reductions.refractive_index = refractive_index
-        dist.parameters4reductions.wavelength = wavelength
-        dist.parameters4reductions.growth_distribution = tdmahyg.hyg_distributions_d200nm
-        fRH = dist.hygroscopicity._get_f_RH(RH_dry, RH_wet)
+        dist = tdmaapssize.size_distribution.copy()
+        # dist = tdmaapssize.size_distribution.zoom_diameter(end=dcoff)
+
+        # dist.parameters4reductions.refractive_index = refractive_index
+        # dist.parameters4reductions.wavelength = wavelength
+        # dist.parameters4reductions.growth_distribution = tdmahyg.hyg_distributions_d200nm
+        # fRH = dist.hygroscopicity._get_f_RH(RH_dry, RH_wet)
+
+        # mode_analysis = True
+        if mode_analysis:
+            refractive_index_aiken_accu, refractive_index_coarse = refractive_index
+            # refractive_index_aiken_accu =refractive_index
+            # refractive_index_coarse = refractive_index
+            # kappa_aiken_accu, kappa_coarse = kappa
+
+            # mode selection and parameter setting
+            dist = dist.convert2dVdlogDp()
+            ## aiken accu
+            dist_aiken_accu = dist.mode_analysis.size_dist_accu + dist.mode_analysis.size_dist_aiken
+            dist_aiken_accu = dist_aiken_accu.zoom_diameter(end=dcoff)
+            dist_aiken_accu.parameters4reductions.refractive_index = refractive_index_aiken_accu
+            dist_aiken_accu.parameters4reductions.wavelength = wavelength
+            dist_aiken_accu.parameters4reductions.growth_distribution = tdmahyg.hyg_distributions_d200nm
+
+            ## coarse
+            dist_coarse = dist.mode_analysis.size_dist_coarse
+            dist_coarse = dist_coarse.zoom_diameter(end=dcoff)
+            dist_coarse.parameters4reductions.refractive_index = refractive_index_coarse
+            dist_coarse.parameters4reductions.wavelength = wavelength
+            dist_coarse.parameters4reductions.kappa = kappa_coarse
+
+            #scattcoeff
+            ## aiken_accu
+            ### dry
+            dist_aiken_accu.parameters4reductions.RH = RH_dry
+            scattcoeff_dry_aiken_accu = dist_aiken_accu.hygroscopicity.grown_size_distribution.optical_properties.scattering_coeff
+            ### wet
+            dist_aiken_accu.parameters4reductions.RH = RH_wet
+            scattcoeff_wet_aiken_accu = dist_aiken_accu.hygroscopicity.grown_size_distribution.optical_properties.scattering_coeff
+
+            ## coarse
+            ### dry
+            dist_coarse.parameters4reductions.RH = RH_dry
+            scattcoeff_dry_coarse = dist_coarse.hygroscopicity.grown_size_distribution.optical_properties.scattering_coeff
+            ### wet
+            dist_coarse.parameters4reductions.RH = RH_wet
+            scattcoeff_wet_coarse = dist_coarse.hygroscopicity.grown_size_distribution.optical_properties.scattering_coeff
+
+            fRH = (scattcoeff_wet_coarse + scattcoeff_wet_aiken_accu) / (scattcoeff_dry_coarse + scattcoeff_dry_aiken_accu)
+
+
+        else:
+            dist = dist.zoom_diameter(end=dcoff)
+            dist.parameters4reductions.refractive_index = refractive_index
+            dist.parameters4reductions.wavelength = wavelength
+            dist.parameters4reductions.growth_distribution = tdmahyg.hyg_distributions_d200nm
+            fRH = dist.hygroscopicity._get_f_RH(RH_dry, RH_wet)
+        return fRH
+
         return fRH
 
     def calculate_new(self):
@@ -167,13 +238,29 @@ class Product(object):
                     print('tdmahyg file missing .... continue')
                 continue
 
-            ior = str(self.refractive_index).replace('.','o')
-            name_addon = '{}_hyg{:d}_rh{:d}v{:d}_ior{}_{}'.format(self.diameter_cutoff,
-                                                     self.hygroscopicity_diameter,
-                                                     self.RH_wet,
-                                                     self.RH_dry,
-                                                     ior,
-                                                     self.data_quality)
+
+
+            if self.mode_analysis:
+                name_addon = '{}_hyg{:d}_rh{:d}v{:d}_ior{}_{}_kc{}_{}'.format(self.diameter_cutoff,
+                                                                      self.hygroscopicity_diameter,
+                                                                      self.RH_wet,
+                                                                      self.RH_dry,
+                                                                      self.refractive_index,
+                                                                      '{}j{}'.format(
+                                                                               self.refractive_index_coarse.real,
+                                                                               self.refractive_index_coarse.imag),
+                                                                      self.kappa_coarse,
+                                                                      self.data_quality)
+
+                name_addon = name_addon.replace('.','o')
+            else:
+                ior = str(self.refractive_index).replace('.', 'o')
+                name_addon = '{}_hyg{:d}_rh{:d}v{:d}_ior{}_{}'.format(self.diameter_cutoff,
+                                                         self.hygroscopicity_diameter,
+                                                         self.RH_wet,
+                                                         self.RH_dry,
+                                                         ior,
+                                                         self.data_quality)
 
             splitname = _splitup_filename(fname_tdmaapssize)
             site = splitname['site']
@@ -203,8 +290,12 @@ class Product(object):
             fname_others = _get_other_filenames(fname_tdmaapssize, ['tdmahyg'], all_files_tdmahyg)
             tdmahyg = _atm_arm.read_cdf(self.folder_tdmahyg + fname_others['tdmahyg']['fname'], data_quality=self.data_quality, verbose=verbose)
 
+            if self.mode_analysis:
+                refractive_index = (refractive_index, self.refractive_index_coarse)
+
             fofrh = self._calculate_one(tdmaapssize, tdmahyg, refractive_index, diameter_cutoff=self.diameter_cutoff, wavelength= self.wavelength,
-                                        hygroscopicity_diameter=self.hygroscopicity_diameter, RH_dry=self.RH_dry, RH_wet=self.RH_wet)
+                                        hygroscopicity_diameter=self.hygroscopicity_diameter, RH_dry=self.RH_dry, RH_wet=self.RH_wet,
+                                        mode_analysis= self.mode_analysis, kappa_coarse= self.kappa_coarse)
 
             if self.keep_data:
                 fofrh_list.append(fofrh)
@@ -218,6 +309,8 @@ class Product(object):
                 self.result = fofrh_cat.close_gaps(verbose=False)
             else:
                 self.result = fofrh_list[0]
+
+
 
 
 def sgptdmaapstdmahyg2fofrh_1um_hyg200_rh85v0_ioraosacsm_patchy(test = False):
@@ -282,4 +375,40 @@ def sgptdmaapstdmahyg2fofrh_1um_hyg200_rh85v0_ior1o5_patchy(test = False):
                         folder_tdmahyg='/Users/htelg/data/ARM/SGP/tdmahyg/',
                         keep_data=False,
                         test=test)
+    return out
+
+def sgptdmaapstdmahyg2fofrh_10um_hyg200_rh85v0_ioraosacsm_1o63j0o01_kc0o0001_patchy(test = False):
+    out = Product(data_quality='patchy',
+                           diameter_cutoff='10um',
+                           hygroscopicity_diameter=200,
+                           wavelength=550,
+                           RH_dry=0,
+                           RH_wet=85,
+                           refractive_index=('aosacsm', complex(1.63, 0.01)),
+                           mode_analysis=True,
+                           kappa_coarse=0.0001,
+                           folder_out='/Volumes/HTelg_4TB_Backup/arm_data/my_products/fRH/tdmaaps_tdmahyg_acsm/10um_hyg200_rh85v0_ioraosacsm_1o63j0o01_kc0o0001_patchy/',
+                           folder_tdmaaps='/Users/htelg/data/ARM/SGP/tdmaaps/',
+                           folder_acsm='/Users/htelg/data/ARM/SGP/acsm/',
+                           folder_tdmahyg='/Users/htelg/data/ARM/SGP/tdmahyg/',
+                           keep_data=False,
+                           test=test)
+    return out
+
+def sgptdmaapstdmahyg2fofrh_1um_hyg200_rh85v0_ioraosacsm_1o63j0o01_kc0o0001_patchy(test = False):
+    out = Product(data_quality='patchy',
+                          diameter_cutoff='1um',
+                          hygroscopicity_diameter=200,
+                          wavelength=550,
+                          RH_dry=0,
+                          RH_wet=85,
+                          refractive_index=('aosacsm', complex(1.63, 0.01)),
+                          mode_analysis=True,
+                          kappa_coarse=0.0001,
+                          folder_out='/Volumes/HTelg_4TB_Backup/arm_data/my_products/fRH/tdmaaps_tdmahyg_acsm/1um_hyg200_rh85v0_ioraosacsm_1o63j0o01_kc0o0001_patchy/',
+                          folder_tdmaaps='/Users/htelg/data/ARM/SGP/tdmaaps/',
+                          folder_acsm='/Users/htelg/data/ARM/SGP/acsm/',
+                          folder_tdmahyg='/Users/htelg/data/ARM/SGP/tdmahyg/',
+                          keep_data=False,
+                          test=test)
     return out
